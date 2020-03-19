fe129052d42ddfd510962e45f01b5a222752e407
vinay <vinay.hpatil@gmail.com>
2020-03-19 22:53:39 +0530

    tmux folder add, this folder contains the tmux plugin

--- index.sh
+++ index.sh
@@ -11,19 +11,28 @@ VIMRC=$HOME/.vimrc
 ZSHRC=$HOME/.zshrc
 TMUX_CONF=$HOME/.tmux.conf
 OH_MY_ZSH=$HOME/.oh-my-zsh
+TMUX=$HOME/.tmux
+P10K=$HOME/.p10k.zsh
 
   cp $VIMRC $HOME/.vimrc.$TS
   cp $TMUX_CONF $HOME/.tmux.conf.$TS
   cp $ZSHRC $HOME/.zshrc.$TS
+  cp $P10K $HOME/.p10k.zsh.$TS
   mv $OH_MY_ZSH $OH_MY_ZSH-$TS
+  mv $TMUX $TMUX-$TS
+
   rm $VIMRC
   rm $TMUX_CONF
   rm $ZSHRC
+  rm $TMUX
+  rm $P10K
   
   ln -s $PWD/vimrc $VIMRC
   ln -s $PWD/tmux.conf $TMUX_CONF
   ln -s $PWD/zshrc $ZSHRC
+  ln -s $PWD/.p10k.zsh $P10K
   ln -s $PWD/oh-my-zsh $OH_MY_ZSH 
+  ln -s $PWD/tmux $TMUX 
 
 
 
