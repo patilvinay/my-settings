8dfd864e1e4e67cf2065049b6dc6e8c6b400bbd7
vinay <vinay.hpatil@gmail.com>
2020-03-19 20:02:04 +0530

    oh-my-sh linking to this directory now

--- index.sh
+++ index.sh
@@ -10,10 +10,12 @@ HOME=~
 VIMRC=$HOME/.vimrc
 ZSHRC=$HOME/.zshrc
 TMUX_CONF=$HOME/.tmux.conf
+OH_MY_ZSH=$HOME/.oh-my-zsh
 
   cp $VIMRC $HOME/.vimrc.$TS
   cp $TMUX_CONF $HOME/.tmux.conf.$TS
   cp $ZSHRC $HOME/.zshrc.$TS
+  mv $OH_MY_ZSH $OH_MY_ZSH-$TS
   rm $VIMRC
   rm $TMUX_CONF
   rm $ZSHRC
@@ -21,6 +23,7 @@ TMUX_CONF=$HOME/.tmux.conf
   ln -s $PWD/vimrc $VIMRC
   ln -s $PWD/tmux.conf $TMUX_CONF
   ln -s $PWD/zshrc $ZSHRC
+  ln -s $PWD/oh-my-zsh $OH_MY_ZSH 
 
 
 
